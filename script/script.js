f11f92fa2a5b62aa4071462b4a0fcff12689fb6a
paul <paul604@users.noreply.github.com>
2016-09-19 15:30:35 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 function set() {
-        var is = document.getElementsByClassName('mute')[0].id;
+        var is = document.getElementsByClassName('../index.html mute')[0].id;
         if (is === "on") {
-          document.getElementsByClassName('mute')[0].id = "off";
-          document.getElementsByTagName("audio")[0].pause();
+          document.getElementsByClassName('../index.html mute')[0].id = "off";
+          document.getElementsByTagName("../index.html audio")[0].pause();
         } else {
-          document.getElementsByClassName('mute')[0].id = "on";
-          document.getElementsByTagName("audio")[0].play();
+          document.getElementsByClassName('../index.html mute')[0].id = "on";
+          document.getElementsByTagName("../index.html audio")[0].play();
         }
       }
